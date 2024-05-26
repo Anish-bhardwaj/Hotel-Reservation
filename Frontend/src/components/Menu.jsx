219aa1fcffeab26bc06419cbadb18ccaad92f5c9
ANISH BHARDWAJ <108463492+Anish-bhardwaj@users.noreply.github.com>
2024-05-26 13:25:32 +0530

    Update Menu.jsx

--- Frontend/src/components/Menu.jsx
+++ Frontend/src/components/Menu.jsx
@@ -7,7 +7,7 @@ const Menu = () => {
         <div className="container">
             <div className="heading_section">
                 <h1 className="heading">POPULAR DISHES</h1>
-                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, iusto dolorem! Voluptatibus ipsum nam mollitia architecto. Soluta pariatur eius et recusandae veritatis. Quasi, et molestias!</p>
+                <p> Delight in our chef's specialties, beloved by guests for their exceptional flavors and unforgettable dining experiences. Here are some amazing dishes of Sandwich Slinger!</p>
             </div>
             <div className="dishes_container">
                 {
