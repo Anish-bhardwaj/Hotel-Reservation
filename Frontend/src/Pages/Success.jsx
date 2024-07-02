674b1a0fc53a398613e5671d778e6d3c5d38ed8b
ANISH BHARDWAJ <108463492+Anish-bhardwaj@users.noreply.github.com>
2024-07-02 07:43:28 +0530

    Update Success.jsx

--- Frontend/src/Pages/Success.jsx
+++ Frontend/src/Pages/Success.jsx
@@ -17,7 +17,42 @@ const Success = () => {
     }, 1000);
     return () => clearInterval(timeoutId);
   }, [navigate]);
+ /*useEffect(() => {
+    if (play) {
+      setCountdown(10);
+      setWin("");
 
+      // Pick four random colors
+      const shuffledColors = colors.sort(() => 0.5 - Math.random());
+      const selectedColors = shuffledColors.slice(0, 4);
+      const sidesWithColors = [
+        { side: 'top', color: selectedColors[0].hex },
+        { side: 'right', color: selectedColors[1].hex },
+        { side: 'bottom', color: selectedColors[2].hex },
+        { side: 'left', color: selectedColors[3].hex },
+      ];
+      setSides(sidesWithColors);
+
+      // Randomly pick one of the selected sides as the target
+      const randomSide = sidesWithColors[Math.floor(Math.random() * sidesWithColors.length)];
+      setTargetSide(randomSide.side);
+
+      const interval = setInterval(() => {
+        setCountdown(prev => {
+          if (prev > 1) {
+            return prev - 1;
+          } else {
+            clearInterval(interval);
+            setWin('Time\'s up! You lose.');
+            setPlay(false);
+            return 0;
+          }
+        });
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+  }, [play]);*/
   return (
     <>
       <section className="notFound">
